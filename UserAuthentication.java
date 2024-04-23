6ac6cbd23cfb5752ba32fadb7c27af3d0095a6d7
Hayatcs <157055585+Hayatcs@users.noreply.github.com>
2024-04-23 12:02:58 +0200

    Add files via upload

--- UserAuthentication.java
+++ UserAuthentication.java
@@ -1,148 +1,100 @@
-package POO;
+
+package javaapplication3;
 import java.util.HashMap;
 import java.util.Map;
-
-enum Gender {
-    WOMEN,MEN;
-}
-public class UserAuthentication {
-    protected Map<String, String> LoginInfo; // Map to store username (or email) and password
-    protected Map<String, UserInfo> userInfoMap; // Map to store user information
-    protected Map<String, AdminInfo> adminInfoMap; // Map to store admin information
-    protected Gender gender;
-    protected String currentUsername;
-
-    // Méthode pour enregistrer le nom d'utilisateur actuellement connecté
-    public void setCurrentUsername(String username) {
-    this.currentUsername = username;
-    }
-
-    // Méthode pour obtenir le nom d'utilisateur actuellement connecté
-    public String getCurrentUsername() {
-    return currentUsername;
-    }
-    public UserAuthentication() {
-        LoginInfo = new HashMap<>();
-        userInfoMap = new HashMap<>();
-        adminInfoMap = new HashMap<>();
-    }
+import java .sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javaapplication3.UserInfo;
+import javaapplication3.UserInfo.Gender;
+public class UserAuthentication {    
+   //la connection avec database:
+ static Statement ss;  
+static String que;
+ static Connection c;
+static String user="root";
+static String pwd="computerhayat";
+static String url="jdbc:mysql://localhost:3306/hotel";
+//les variables de connection|
 
 
-    // Method to sign up a new user
-    public void SignUp(String username, String password, String name, String familyName, Date date, String address, String email ,String phoneNumber , Gender gender) {
+protected Map<String, String> LoginInfouser; // Map to store username (or email) and password
+protected Map<String, String> LoginInfoAdmin;
+//map to store user information:
+protected Map<String, UserInfo> userInfoMap;
+protected Map<String,AdminInfo>adminInfoMap;
+//constrecteur:
+
+        
+
+
+//le code normale :
+
+ 
+  
+   // Method to sign up a new user
+    public void SignUp(String username, String password, String name, String familyName, Date date, String address, String email, Gender gender, String phoneNumber) {
+       
         // Store username and password
-        LoginInfo.put(username, password);
+        LoginInfouser.put(username, password);
+    
+
         // Store user information
         userInfoMap.put(username, new UserInfo(name, familyName, date, address, email, gender, phoneNumber));
-    }
-
-
-
-     // Method to authenticate user during login
+     try {
+         ss=c.createStatement();
+         que = "INSERT INTO user VALUES ('" + name + "','" + familyName + "','" + date + "','" + address + "','" + email + "','" + gender + "','" + phoneNumber + "','" +password+"')" ;
+        ss.execute(que);
+       
+        String q = "INSERT INTO LoginInfo VALUES ('" + username + "','" + password + "')";
+           ss.execute(q);
+            System.out.println("Insert!");
+     } catch (SQLException ex) {
+         Logger.getLogger(UserAuthentication.class.getName()).log(Level.SEVERE, null, ex);
+     }       
+    }//fin.
+      // Method to authenticate user during login
      public boolean login(String username, String password) {
         // Check if the username exists and if the password matches
-        return LoginInfo.containsKey(username) && LoginInfo.get(username).equals(password);
+        return LoginInfouser.containsKey(username) && LoginInfouser.get(username).equals(password);
     }
-
-
-
-    protected static class UserInfo {
-        protected String name;
-        protected String familyName;
-        protected Date date;
-        protected String address;
-        protected String email;
-        protected String phoneNumber;
-        protected Gender gender;
-
-        public UserInfo(String name, String familyName, Date date, String address, String email , Gender gender, String phoneNumber) {
-            this.name = name;
-            this.familyName = familyName;
-            this.date = date;
-            this.address = address;
-            this.email = email;
-            this.phoneNumber = phoneNumber;
-            this.gender = gender;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getFamilyName() {
-            return familyName;
-        }
-
-        public void setFamilyName(String familyName) {
-            this.familyName = familyName;
-        }
-
-        public Date getDate() {
-            return date;
-        }
-
-        public void setDate(Date date) {
-            this.date = date;
-        }
-
-        public String getAddress() {
-            return address;
-        }
-
-        public void setAddress(String address) {
-            this.address = address;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        public String getPhoneNumber() {
-            return phoneNumber;
-        }
-
-        public void setPhoneNumber(String phoneNumber) {
-            this.phoneNumber = phoneNumber;
-        }
-
-        public Gender getGender() {
-            return gender;
-        }
-
-        public void setGender(Gender gender) {
-            this.gender = gender;
-        }        
-    }
-
-
-
-
-
-     // Method to sign up a new admin
+// Method to sign up a new admin
      public void SignUpAdmin(String username, String password, String name, String familyName, Date date, String address, String email, String phoneNumber, Gender gender, String adminRole) {
         // Store username and password
-        LoginInfo.put(username, password);
+        LoginInfoAdmin.put(username, password);
 
         // Store admin information
         adminInfoMap.put(username, new AdminInfo(name, familyName, date, address, email, gender, phoneNumber,adminRole ));
+         try {
+         ss=c.createStatement();
+         que = "INSERT INTO Admin VALUES ('" + name + "','" + familyName + "','" + date + "','" + address + "','" + email + "','" + gender + "','" + phoneNumber + "','" +password+"','"+adminRole+"')" ;
+        ss.execute(que);
+        String q = "INSERT INTO LoginInfoAdmin VALUES ('" + username + "','" + password + "')";
+           ss.execute(q);
+        System.out.println("inseret !");
+        
+     } catch (SQLException ex) {
+         Logger.getLogger(UserAuthentication.class.getName()).log(Level.SEVERE, null, ex);
+     }
     }
-
-    protected static class AdminInfo extends UserInfo {
-        protected String adminRole;
+     //classe les informations de l'admine :
+       class AdminInfo extends UserInfo {
+        private String adminRole;
         public AdminInfo(String name, String familyName, Date date, String address, String email, Gender gender, String phoneNumber, String adminRole) {
-            super(name, familyName, date, address, email, gender, phoneNumber);
+            super(name, familyName, date, address, email,gender, phoneNumber);
             this.adminRole = adminRole;
         }
         public String getAdminRole() {
             return adminRole;}
 
     }
-}
+  
+  
+ public static void main(String[] args) throws SQLException {
+ //connecter:
+  c=DriverManager.getConnection(url,user,pwd);
+         
+    }
+    
+     }
+
